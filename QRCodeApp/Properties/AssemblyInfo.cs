c2d88feee190b48ae66a9fa31c746fe6370c4bb7
rushaan1 <cocrushaan3@gmail.com>
2024-03-02 00:22:32 +0530

    Added Version info & icon

--- QRCodeApp/Properties/AssemblyInfo.cs
+++ QRCodeApp/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("QRCodeApp")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
